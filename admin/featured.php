ead0c4b1241e6ba4c38ef15a34115353d5fcc616
mc12345678 <mc12345678@users.noreply.github.com>
2022-02-02 18:32:57 -0500

    Update featured.php
    Removed notice causing code and unused variable as no discussion has been had not desire expressed to work as a team to resolve other expressed concerns.

--- admin/featured.php
+++ admin/featured.php
@@ -374,7 +374,6 @@ if (!empty($action)) {
                 // Split Page
                 // reset page when page is unknown
                 if ((empty($_GET['page']) || $_GET['page'] == '1') && !empty($_GET['fID'])) {
-                    $old_page = $_GET['page'];
                     $check_page = $db->Execute($featured_query_raw);
                     if ($check_page->RecordCount() > MAX_DISPLAY_SEARCH_RESULTS_FEATURED_ADMIN) {
                         $check_count = 0;
@@ -385,7 +384,6 @@ if (!empty($action)) {
                             $check_count++;
                         }
                         $_GET['page'] = round((($check_count / MAX_DISPLAY_SEARCH_RESULTS_FEATURED_ADMIN) + (fmod_round($check_count, MAX_DISPLAY_SEARCH_RESULTS_FEATURED_ADMIN) !== 0 ? .5 : 0)));
-                        $page = $_GET['page'];
                     } else {
                         $_GET['page'] = 1;
                     }

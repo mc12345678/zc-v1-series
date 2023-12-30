86395b4e354bc261280c5e3dbc30dbbebfc73afe
mc12345678 <mc12345678@users.noreply.github.com>
2023-12-29 19:01:43 -0500

    Update init_sefu.php
    Provide early return.
    Properly implement for loop incrementing by value of 2.
    Replace array sizeof with count.
    Apply spacing in variable assignment. 
    Replace comment guidance for more information from the old wiki link to the current docs location of the same information. 
    Result of the above is to shift all code to the left and remove nested if and other code.

--- includes/init_includes/init_sefu.php
+++ includes/init_includes/init_sefu.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * set the HTTP GET parameters manually if search_engine_friendly_urls is enabled
- * see {@link  http://www.zen-cart.com/wiki/index.php/Developers_API_Tutorials#InitSystem wikitutorials} for more details.
+ * see {@link  https://docs.zen-cart.com/dev/code/init_system/ docs} for more details.
  *
  * @package initSystem
  * @copyright Copyright 2003-2018 Zen Cart Development Team
@@ -12,23 +12,25 @@
 if (!defined('IS_ADMIN_FLAG')) {
   die('Illegal Access');
 }
-if (SEARCH_ENGINE_FRIENDLY_URLS == 'true') {
-  if (strlen($_SERVER['REQUEST_URI']) > 1) {
-    $GET_array = array();
-    $PHP_SELF = $_SERVER['SCRIPT_NAME'];
-    $vars = explode('/', substr($_SERVER['REQUEST_URI'], 1));
-    for ($i=0, $n=sizeof($vars); $i<$n; $i++) {
-      if (strpos($vars[$i], '[]')) {
-        $GET_array[substr($vars[$i], 0, -2)][] = $vars[$i+1];
-      } else {
-        $_GET[$vars[$i]] = $vars[$i+1];
-      }
-      $i++;
-    }
-    if (sizeof($GET_array) > 0) {
-      foreach($GET_array as $key => $value) {
-        $_GET[$key] = $value;
-      }
-    }
+if (SEARCH_ENGINE_FRIENDLY_URLS !== 'true') {
+  return;
+}
+if (strlen($_SERVER['REQUEST_URI']) <= 1) {
+  return;
+}
+$GET_array = [];
+$PHP_SELF = $_SERVER['SCRIPT_NAME'];
+$vars = explode('/', substr($_SERVER['REQUEST_URI'], 1));
+for ($i=0, $n = count($vars); $i < $n; $i = $i + 2) {
+  if (!empty(strpos($vars[$i], '[]'))) {
+    $GET_array[substr($vars[$i], 0, -2)][] = $vars[$i+1];
+    continue; 
   }
+  $_GET[$vars[$i]] = $vars[$i + 1];
+}
+if (count($GET_array) === 0) {
+  return;
+}
+foreach($GET_array as $key => $value) {
+  $_GET[$key] = $value;
 }
